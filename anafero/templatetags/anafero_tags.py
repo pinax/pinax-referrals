14d06d1143a551288114aa6a5c32a8ecc44a36dc
Patrick Altman <paltman@gmail.com>
2012-06-21 11:10:18 -0500

    Whitespace nit

--- anafero/templatetags/anafero_tags.py
+++ anafero/templatetags/anafero_tags.py
@@ -12,5 +12,3 @@ def create_referral(url, obj=None):
         return {"url": url, "obj": obj, "obj_ct": ContentType.objects.get_for_model(obj)}
     else:
         return {"url": url, "obj": "", "obj_ct": ""}
-
-    
