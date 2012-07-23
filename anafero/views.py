40e64bc7b33edf2812519b2c88a8a94b1dff41a6
Patrick Altman <paltman@gmail.com>
2012-07-23 10:00:15 -0500

    Add support for bootstrap-ajax.js
    bootstrap-ajax.js is a library to support declarative
    ajax functionality written by Eldarion. It will soon
    be released as open source.

--- anafero/views.py
+++ anafero/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import redirect, get_object_or_404
 from django.utils import simplejson as json
+from django.template import RequestContext
+from django.template.loader import render_to_string
 from django.views.decorators.http import require_POST
 
 from django.contrib.auth.decorators import login_required
@@ -13,9 +15,13 @@ from anafero.models import Referral
 @require_POST
 def create_referral(request):
     target = None
+    ctx = {"url": request.POST.get("redirect_to")}
+    
     if request.POST.get("obj_ct_pk") and request.POST.get("obj_pk"):
         ct = ContentType.objects.get(pk=request.POST.get("obj_ct_pk"))
         target = ct.get_object_for_this_type(pk=request.POST.get("obj_pk"))
+        ctx["obj"] = target
+        ctx["obj_ct"] = ct
     
     referral = Referral.create(
         user=request.user,
@@ -27,7 +33,12 @@ def create_referral(request):
     return HttpResponse(
         json.dumps({
             "url": referral.url,
-            "code": referral.code
+            "code": referral.code,
+            "html": render_to_string(
+                "anafero/_create_referral_form.html",
+                ctx,
+                context_instance=RequestContext(request)
+            )
         }),
         mimetype="application/json"
     )
