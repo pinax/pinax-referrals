07aaa98abddc38bf6c715bb71bf700f87cd93a53
Patrick Altman <paltman@gmail.com>
2014-07-11 01:04:46 -0400

    Fix copy and paste error

--- runtests.py
+++ runtests.py
@@ -41,7 +41,7 @@ def runtests(*test_args):
     try:
         from django.test.runner import DiscoverRunner
         runner_class = DiscoverRunner
-        test_args = ["kaleo.tests"]
+        test_args = ["anafero.tests"]
     except ImportError:
         from django.test.simple import DjangoTestSuiteRunner
         runner_class = DjangoTestSuiteRunner
