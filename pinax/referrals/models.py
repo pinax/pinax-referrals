e008d0260c40e2d576d630de0afb67577440d32f
Patrick Altman <paltman@gmail.com>
2015-02-21 07:04:29 -0600

    Make expiry blankable

--- pinax/referrals/models.py
+++ pinax/referrals/models.py
@@ -24,7 +24,7 @@ class Referral(models.Model):
     )
     label = models.CharField(max_length=100, blank=True)
     code = models.CharField(max_length=40, unique=True)
-    expired_at = models.DateTimeField(null=True)
+    expired_at = models.DateTimeField(null=True, blank=True)
     redirect_to = models.CharField(max_length=512)
     target_content_type = models.ForeignKey(ContentType, null=True, blank=True)
     target_object_id = models.PositiveIntegerField(null=True, blank=True)
