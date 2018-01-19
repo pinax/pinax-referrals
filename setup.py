3d27c3e5fa063b0a8893ce8dffb5a351ee724a2f
KatherineMichel <kthrnmichel@gmail.com>
2018-01-19 17:59:01 -0600

    Fixing license badge target

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ Pinax Referrals
 .. image:: http://slack.pinaxproject.com/badge.svg
     :target: http://slack.pinaxproject.com/
 .. image:: https://img.shields.io/badge/license-MIT-blue.svg
-    :target: https://pypi.python.org/pypi/pinax-referrals/
+    :target: https://opensource.org/licenses/MIT/
 
 \ 
     
