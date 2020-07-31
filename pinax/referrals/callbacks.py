54bfdaab5ecda70b7aab1a37adb3a8d24795490a
Assistentus <65783789+Assistentus@users.noreply.github.com>
2020-07-31 15:13:01 +0300

    Update callbacks.py
    fixed a typo

--- pinax/referrals/callbacks.py
+++ pinax/referrals/callbacks.py
@@ -3,7 +3,7 @@ import random
 
 def generate_code(referral_class):
     def _generate_code():
-        t = "abcdefghijkmnopqrstuvwwxyzABCDEFGHIJKLOMNOPQRSTUVWXYZ1234567890"
+        t = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLOMNOPQRSTUVWXYZ1234567890"
         return "".join([random.choice(t) for i in range(40)])
     code = _generate_code()
     while referral_class.objects.filter(code=code).exists():
